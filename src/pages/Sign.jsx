8aa929d092395552c6c0f8fa6d984521864ee3b8
Pedro Pinho <rihor1000@gmail.com>
2020-12-08 14:04:50 -0300

    Connection Sign page to firebase

--- src/pages/Sign.jsx
+++ src/pages/Sign.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+
 import { Container } from "./styles/shared";
 import { Form, Title } from "./styles/sign";
 
+import { useSession } from "../stores/Session";
+
 function SignPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useSession();
+
+  async function handleSignIn(event) {
+    event.preventDefault();
+
+    console.log("handleSignIn", email, password);
+
+    await signIn({ email, password });
+  }
+
   return (
     <Container role="main">
       <Title>Sign in</Title>
@@ -9,17 +26,29 @@ function SignPage() {
         <div>
           <label>Email</label>
           <div>
-            <input type="email" />
+            <input
+              type="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
           </div>
           <span>your@email.com</span>
         </div>
         <div>
           <label>Password</label>
           <div>
-            <input type="password" />
+            <input
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
           </div>
           <span>At least six characters</span>
         </div>
+
+        <button type="submit" onClick={handleSignIn}>
+          Enter
+        </button>
       </Form>
     </Container>
   );
