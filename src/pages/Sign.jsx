0d135f1e50b8cc4f9dc6b6d0c6d548df30924696
Pedro Pinho <rihor1000@gmail.com>
2020-12-09 13:52:46 -0300

    Change SignIn page to look like Create note page

--- src/pages/Sign.jsx
+++ src/pages/Sign.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
 
-import { Container } from "./styles/shared";
-import { Form, Title } from "./styles/sign";
+import Input from "../components/form/Input";
+import Button from "../components/form/Button";
+import {
+  Background,
+  GreenBackground,
+  WhiteBackground,
+  MainContainer,
+} from "./styles/sign";
 
 import { useSession } from "../stores/Session";
 
@@ -14,43 +20,64 @@ function SignPage() {
   async function handleSignIn(event) {
     event.preventDefault();
 
-    console.log("handleSignIn", email, password);
+    if (!email || !password) {
+      return;
+    }
 
     await signIn({ email, password });
   }
 
   return (
-    <Container role="main">
-      <Title>Sign in</Title>
-      <Form>
-        <div>
-          <label>Email</label>
-          <div>
-            <input
-              type="email"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-            />
-          </div>
-          <span>your@email.com</span>
-        </div>
-        <div>
-          <label>Password</label>
-          <div>
-            <input
-              type="password"
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-            />
-          </div>
-          <span>At least six characters</span>
-        </div>
+    <Background>
+      <GreenBackground></GreenBackground>
+      <WhiteBackground></WhiteBackground>
+      <MainContainer>
+        <h1>Sign in</h1>
 
-        <button type="submit" onClick={handleSignIn}>
-          Enter
-        </button>
-      </Form>
-    </Container>
+        <form onSubmit={handleSignIn}>
+          <Input
+            name="email"
+            value={email}
+            setValue={setEmail}
+            label="Email"
+            required
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M14.243 5.757a6 6 0 10-.986 9.284 1 1 0 111.087 1.678A8 8 0 1118 10a3 3 0 01-4.8 2.401A4 4 0 1114 10a1 1 0 102 0c0-1.537-.586-3.07-1.757-4.243zM12 10a2 2 0 10-4 0 2 2 0 004 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </Input>
+          <Input
+            name=""
+            value={password}
+            setValue={setPassword}
+            label="Password"
+            required
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </Input>
+
+          <Button type="submit">Enter</Button>
+        </form>
+      </MainContainer>
+    </Background>
   );
 }
 
--- src/pages/styles/sign.js
+++ src/pages/styles/sign.js
@@ -1,31 +1,82 @@
 import styled from "styled-components";
 
-export const Title = styled.h1`
-  color: #fefefe;
-  margin-bottom: 50px;
+// export const Title = styled.h1`
+//   color: #fefefe;
+//   margin-bottom: 50px;
+// `;
+
+export const Background = styled.div`
+  height: 100%;
+  position: relative;
 `;
 
-export const Form = styled.form`
+export const GreenBackground = styled.div`
+  background: #48bb78;
+  width: 100%;
+  height: 50%;
+`;
+
+export const WhiteBackground = styled.div`
   background: #fefefe;
-  padding: 20px 40px;
-  border-radius: 8px;
-  box-shadow: 2px 4px 16px rgba(0, 0, 0, 0.1);
+  width: 100%;
+  height: 50%;
+`;
 
-  div {
-    & + div {
-      margin-top: 10px;
+export const MainContainer = styled.main`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
+    0 4px 6px -2px rgba(0, 0, 0, 0.05);
+
+  width: 40%;
+  padding: 12px 24px;
+  background-color: #fefefe;
+  border: 1px solid #48bb78;
+  border-radius: 0.375rem;
+  transition: 0.2s;
+
+  h1 {
+    color: #358f5a;
+    font-size: clamp(2rem, 2.5rem, 3rem);
+  }
+
+  @media only screen and (max-width: 1000px) {
+    & {
+      width: 85%;
     }
+  }
 
-    input {
-      padding: 4px 8px;
-      border-radius: 4px;
-      border: none;
-      background: #f0f0f0;
-    }
-
-    span {
-      font-size: 0.8rem;
-      opacity: 0.8;
+  @media only screen and (max-width: 800px) {
+    & {
+      width: 90%;
     }
   }
 `;
+
+// export const Form = styled.form`
+//   background: #fefefe;
+//   padding: 20px 40px;
+//   border-radius: 8px;
+//   box-shadow: 2px 4px 16px rgba(0, 0, 0, 0.1);
+
+//   div {
+//     & + div {
+//       margin-top: 10px;
+//     }
+
+//     input {
+//       padding: 4px 8px;
+//       border-radius: 4px;
+//       border: none;
+//       background: #f0f0f0;
+//     }
+
+//     span {
+//       font-size: 0.8rem;
+//       opacity: 0.8;
+//     }
+//   }
+// `;
