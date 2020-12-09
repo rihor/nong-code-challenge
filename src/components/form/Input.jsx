bbbe2f286bbd5b9da69920d8df46953183d901a9
Pedro Pinho <rihor1000@gmail.com>
2020-12-09 13:52:24 -0300

    Refactoring input component and permiting svg icons

--- src/components/form/Input.jsx
+++ src/components/form/Input.jsx
@@ -1,15 +1,56 @@
-import { Container } from "./styles";
+import styled from "styled-components";
 
-const Input = ({ value, setValue, label, name, type }) => {
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  label {
+    margin-top: 6px;
+    margin-bottom: 2px;
+  }
+
+  > div {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    border-radius: 4px;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    background: rgba(0, 0, 0, 0.05);
+
+    svg {
+      margin-left: 4px;
+      width: 16px;
+      height: 16px;
+      flex: none;
+      color: rgba(0, 0, 0, 0.6);
+    }
+
+    input {
+      border: none;
+      width: 100%;
+      background: transparent;
+      padding: 4px 8px;
+    }
+  }
+
+  > div:hover {
+    border: 1px solid rgba(0, 0, 0, 0.2);
+  }
+`;
+
+const Input = ({ value, setValue, label, name, type, children }) => {
   return (
     <Container>
       <label htmlFor={name}>{label}</label>
-      <input
-        type={type || "text"}
-        id={name}
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
-      />
+      <div>
+        {children}
+        <input
+          type={type || "text"}
+          id={name}
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+        />
+      </div>
     </Container>
   );
 };
--- src/components/form/Textarea.jsx
+++ src/components/form/Textarea.jsx
@@ -1,4 +1,26 @@
-import { Container } from "./styles";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  label {
+    margin-top: 6px;
+    margin-bottom: 2px;
+  }
+
+  textarea {
+    background: rgba(0, 0, 0, 0.05);
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    padding: 4px 8px;
+    resize: vertical;
+    max-height: 300px;
+  }
+
+  textarea:hover {
+    border: 1px solid rgba(0, 0, 0, 0.2);
+  }
+`;
 
 const Textarea = ({ value, setValue, label, name }) => {
   return (
